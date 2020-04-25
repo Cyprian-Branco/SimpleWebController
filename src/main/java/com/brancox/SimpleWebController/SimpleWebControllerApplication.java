c01ef0099aaddd77f3e4ce9e8caa77d5c4a64830
Cyprian Bowen <branco1lelgo@gmail.com>
2020-04-25 14:01:54 +0300

    initial commit

---
+++ src/main/java/com/brancox/SimpleWebController/SimpleWebControllerApplication.java
@@ -0,0 +1,13 @@
+package com.brancox.SimpleWebController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebControllerApplication.class, args);
+	}
+
+}
